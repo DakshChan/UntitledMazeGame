97858b9129c77193186eb2792729543fb6fd2324
Aryan <aryanabed48@gmail.com>
2020-06-18 22:03:44 -0400

    Added Basic Maze Generator

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
 	public static void main(String[] args) {
+
+		//MazeGenerator mazeGenerator = new MazeGenerator();
 		Gui g = new Gui();
 		
 		//Threading here
---
+++ src/MazeGenerator.java
@@ -0,0 +1,103 @@
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+public class MazeGenerator {
+	final int HEIGHT = 11;
+	final int WIDTH = 11;
+
+	enum Directions {
+		UP,
+		DOWN,
+		LEFT,
+		RIGHT
+	}
+
+	float[][] maze = new float[HEIGHT][WIDTH];
+
+	MazeGenerator() {
+		for(int i = 0; i < HEIGHT; i++) {
+			for(int j = 0; j < WIDTH; j++) {
+				if (i % 2 == 1 || j % 2 == 1) {
+					maze[i][j] = 0;
+				}
+				if (i == 0 || i == HEIGHT - 1 || j == 0 || j == WIDTH - 1) {
+					maze[i][j] = 0.5f;
+				}
+			}
+		}
+
+		// an even random number from 2 to WIDTH - 2 exclusive
+		Random random = new Random();
+		int sx = 2 + random.nextInt((WIDTH - 4) / 2) * 2;
+		int sy = 2 + random.nextInt((HEIGHT - 4) / 2) * 2;
+		generator(sx, sy, maze);
+
+		for (int i = 0; i < HEIGHT; i++) {
+			for (int j = 0; j < WIDTH; j++) {
+				if (maze[i][j] == 0.5f) {
+					maze[i][j] = 1.0f;
+				}
+			}
+		}
+
+		maze[1][2] = 1.0f;
+		maze[HEIGHT - 2][WIDTH - 3] = 1.0f;
+	}
+
+	private void generator(int cx, int cy, float[][] grid) {
+		grid[cy][cx] = 0.5f;
+
+		Random random = new Random();
+
+		if (grid[cy - 2][cx] == 0.5f && grid[cy + 2][cx] == 0.5f && grid[cy][cx - 2] == 0.5f && grid[cy][cx + 2] == 0.5f) {
+			// has no where to go
+		} else {
+			ArrayList<Directions> directions = new ArrayList<>(Arrays.asList(Directions.UP, Directions.DOWN, Directions.LEFT, Directions.RIGHT));
+			while (directions.size() > 0) {
+				Directions direction = directions.get(random.nextInt(directions.size()));
+				directions.remove(direction);
+
+				int ny, nx, my, mx;
+
+				if (direction == Directions.UP) {
+					ny = cy - 2;
+					my = cy - 1;
+				} else if (direction == Directions.DOWN) {
+					ny = cy + 2;
+					my = cy + 1;
+				} else {
+					ny = cy;
+					my = cy;
+				}
+
+				if (direction == Directions.LEFT) {
+					nx = cx - 2;
+					mx = cx - 1;
+				} else if (direction == Directions.RIGHT) {
+					nx = cx + 2;
+					mx = cx + 1;
+				} else {
+					nx = cx;
+					mx = cx;
+				}
+
+				if (grid[ny][nx] != 0.5f) {
+					grid[my][mx] = 0.5f;
+					generator(nx, ny, grid);
+				}
+			}
+		}
+	}
+
+/*	public void showMaze() {
+		for (int i = 0; i < HEIGHT; i++) {
+			for (int j = 0; j < WIDTH; j++) {
+				System.out.print(maze[i][j] + " ");
+			}
+			System.out.println("");
+		}
+	}*/
+
+}
